c4d3f0d507d033dfac3580f6bb795c70e632debc
T150870h <90712503+T150870h@users.noreply.github.com>
2021-12-14 19:26:10 +0700

    hethong

--- buoi8/buoi9.js
+++ buoi8/buoi9.js
@@ -1,11 +1,32 @@
-const form = document.querySelector('#id');
-const usernameInput = form.querySelector('#username');
-const passwordInput = form.querySelector('#password');
-  form.addEventListener('submit', (e) => {
+const form = document.querySelector('#loginForm');
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+// const listUser = [
+//   {
+//     username: 'admin',
+//     password: 'admin',
+//   },
+//   {
+//     username: 'hung',
+//     password: 'hung',
+//   }
+// ];
+//    localStorage.setItem('listUser', JSON.stringify(listUser));  // localStorage.setItem('') dùng để lưu biến vào localStorage
+     // JSON.stringify()   đổi code oj sang string
+     // setItem gán vào localStorage
+   form.addEventListener('submit', (e) => {
          e.preventDefault()
          const username = usernameInput.value;
          const password = passwordInput.value;
-            if( username === 'admin' && password === 'admin' ) {
-              window.location.href = './buoi8.html'
-            };
+         const listUser = JSON.parse(localStorage.getItem('listUser'));
+          // JSON.parse đổi từ string sang oj bình thường
+          // getItem lấy ra localStorage
+          const check = listUser.findIndex((item) => item.username == username && item.password == password)  
+              // đk || một trong 2 điều kiện đúng thì sẽ đúng
+              // đk && cả 2 đk đúng mới đúng
+            if(check >= 0  ) {
+              window.location.href = './buoi8.html';
+            } else {
+              alert('Nhập sai mật khẩu và tên đăng nhập')
+            }
   })
